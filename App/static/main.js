109ad3d66b8c0d233316cdee4227ca2baefd9433
Harjappan Singh <singhharjappan@gmail.com>
2024-10-31 18:52:29 +0000

    ✨ Configured Flask App to use pubnub realtime updates

--- App/static/main.js
+++ App/static/main.js
@@ -1,5 +1,7 @@
 let aliveSecond = 0;
 let heartBeatRate = 5000;
+let pubnub;
+let appChannel = "parksmart_pi_channel";
 
 function time()
 {
@@ -27,16 +29,6 @@ function keepAlive()
         }
         throw new Error("Server offline");
     })
-    .then(responseJson=>{
-        if(responseJson.parking_space == 1)
-        {
-            document.getElementById("space1_id").innerHTML="Booked/Occupied";
-        }
-        else
-        {
-            document.getElementById("space1_id").innerHTML="Available";
-        }
-    })
     .catch(error=>console.log(error));
     setTimeout('keepAlive()', heartBeatRate);
 }
@@ -46,17 +38,54 @@ function handleClick(cb)
     let value;
     if (cb.id === "green_led") {
         value = "off"; // Interpret green_led selection as turning off the red_led
-        sendEvent("red_led-" + value);
+        // sendEvent("red_led-" + value);
     } else {
         value = cb.checked ? "on" : "off";
-        sendEvent(cb.id + "-" + value);
+        // sendEvent(cb.id + "-" + value);
     }
+    publishMessage({"LED" : value})
 }
 
-function sendEvent(value)
+const setupPubNub = () => {
+    pubnub = new PubNub({
+        publishKey: 'pub-c-34b92735-3dad-43e4-b3a6-1b0634db6003',
+        subscribeKey: 'sub-c-af85c9fa-2327-45c8-accc-1b7a929001dc',
+        userId: "harjappansingh_mac",
+    });
+
+    //create a channel
+    const channel = pubnub.channel(appChannel);
+
+    //create a subscription
+    const subscription = channel.subscription();
+
+    pubnub.addListener({
+        status: (s) =>{
+            console.log('Status', s.category);
+        },
+    });
+
+    subscription.onMessage = (messageEvent) => {
+        handleMessage(messageEvent.message);
+    };
+
+    subscription.subscribe();
+};
+
+const publishMessage = async(message) => {
+    const publishPayload = {
+        channel : appChannel,
+        message : message,
+    };
+    await pubnub.publish(publishPayload);
+};
+
+function handleMessage(message)
 {
-    fetch("/status="+value,
-    {
-        method:"POST",
-    })
+    console.log(message)
+    if (message["Occupied"] === "Yes") {
+        document.getElementById("space1_id").innerHTML = "Booked";
+    } else if (message["Occupied"] === "No") {
+        document.getElementById("space1_id").innerHTML = "Available";
+    }
 }
