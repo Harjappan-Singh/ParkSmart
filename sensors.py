24e7d4f4e5ff5525e4e9d9651feb6abe266f35fa
Harjappan Singh <singhharjappan@gmail.com>
2024-12-21 00:29:08 +0000

    ✨ Add logic to monitor vehicle count using IR Break Beam
    Used threads to start and stop monitoring irrespective of the other commands like turning on/off LED

--- sensors.py
+++ sensors.py
@@ -1,14 +1,20 @@
 import RPi.GPIO as GPIO  
 import time
 import json
+import threading
 
 RED_LED_PIN = 26
 GREEN_LED_PIN = 16
+IR_SENSOR_PIN = 25
+
+vehicle_count = 0
+monitoring_active = False
 
 def setup_gpio():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(RED_LED_PIN, GPIO.OUT)
     GPIO.setup(GREEN_LED_PIN, GPIO.OUT)
+    GPIO.setup(IR_SENSOR_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def read_input_command(msg):
     if msg == "{'redLed': 'on'}":
@@ -25,8 +31,10 @@ def read_input_command(msg):
         turn_off_green_LED()
     elif msg == "{'parkSmartMonitor': 'on'}":
         print("Monitoring start")
+        start_monitoring()
     elif msg == "{'parkSmartMonitor': 'off'}":
         print("Monitoring stopped")
+        stop_monitoring()
     else:
         print("Bad command, Try Again!!!!")
 
@@ -42,6 +50,30 @@ def turn_on_green_LED():
 
 def turn_off_green_LED():
     GPIO.output(GREEN_LED_PIN, GPIO.LOW)
+
+def monitor_vehicle_count():
+    global vehicle_count, monitoring_active
+
+    last_state = GPIO.input(IR_SENSOR_PIN)
+
+    while monitoring_active:
+        current_state = GPIO.input(IR_SENSOR_PIN)
+        if last_state == 1 and current_state == 0:
+            vehicle_count += 1
+            print(f"Current vehicle count: {vehicle_count}")
+        last_state = current_state
+        time.sleep(0.1)
+
+def start_monitoring():
+    global monitoring_active
+    if not monitoring_active:
+        monitoring_active = True
+        threading.Thread(target=monitor_vehicle_count, daemon=True).start()
+
+def stop_monitoring():
+    global monitoring_active
+    monitoring_active = False
+
 
 def main():
     try:
