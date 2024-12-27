48c401f021e57202dcd6026274dabe32a4037dd3
Harjappan Singh <singhharjappan@gmail.com>
2024-12-27 17:13:42 +0000

    🗃️ Define table to store parking lot data

--- Database/park_smart.sql
+++ Database/park_smart.sql
@@ -17,6 +17,40 @@ CREATE TABLE users (
     email VARCHAR(255)
 );
 
+CREATE TABLE parking_lot (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    parking_spot VARCHAR(10) NOT NULL,
+    status VARCHAR(20) NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
 -- Sample Insertion
 INSERT INTO users (id, name, client_id, token, login, read_access, write_access, email)
 VALUES (1, 'John', '109565775953816462122', NULL, 1, 0, 0, 'john@gmail.com');
+
+-- Insert these giving gaps 
+INSERT INTO parking_lot (user_id, parking_spot, status)
+VALUES 
+(2, 'P1', 'occupied');
+
+INSERT INTO parking_lot (user_id, parking_spot, status)
+VALUES 
+(2, 'P2', 'occupied');
+
+INSERT INTO parking_lot (user_id, parking_spot, status)
+VALUES 
+(2, 'P1', 'available');
+
+INSERT INTO parking_lot (user_id, parking_spot, status)
+VALUES 
+(2, 'P3', 'occupied');
+
+INSERT INTO parking_lot (user_id, parking_spot, status)
+VALUES 
+(2, 'P2', 'available');
+
+INSERT INTO parking_lot (user_id, parking_spot, status)
+VALUES 
+(2, 'P3', 'available');
