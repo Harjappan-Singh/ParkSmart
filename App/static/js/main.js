c21cb957e200db9fe70fa10d2eca3202b1cec9bd
Harjappan Singh <singhharjappan@gmail.com>
2024-12-27 17:35:07 +0000

    🐛 Handle dynamic handling of parking space
    Previously it was only handling status for parking space 1 (P1), now it dynamically updates the UI for any parking space.

--- App/static/js/main.js
+++ App/static/js/main.js
@@ -46,24 +46,31 @@ function publishMessage(message, callback) {
 }
 
 function handleIncomingMessage(event) {
-     try {
-        // console.log("Received message:", event.message);
-
+    try {
         const msg = event.message;
 
-        if (msg.P1 !== undefined) {
-            console.log("P1 is", msg.P1);
-            document.querySelectorAll('.space1').forEach(element => {
-                element.innerHTML = msg.P1;
-            });
-        } else if (msg.vehicle_count !== undefined) {
-            console.log("Current vehicle count:", msg.vehicle_count);
-            document.querySelector('#vehicleCount').innerHTML = msg.vehicle_count
+        for (const key in msg) {
+            // Message is parking space
+            if (msg.hasOwnProperty(key) && key.startsWith('P')) { 
+                const value = msg[key];
+                // console.log(`${key} is`, value);
+
+                const className = `.space${key.substring(1)}`;
+                document.querySelectorAll(className).forEach(element => {
+                    element.innerHTML = value;
+                });
+
+            } else if (key === 'vehicle_count') {
+                // Message is vehicle count
+                console.log("Current vehicle count:", msg[key]);
+                document.querySelector('#vehicleCount').innerHTML = msg[key];
+            }
         }
     } catch (error) {
         console.error("Error handling incoming message:", error);
-     }
+    }
 }
+
 
 
 function handlePubNubStatus(statusEvent) {
